4b4814d1885ade1e428e8cba2e26c687563cab98
Chetan Agali <chetand.agali@gmail.com>
2017-06-12 15:28:47 +0530

    path sellting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # before_filter :find_url
+  before_filter :find_url
 
-  # def find_url
-  # 	@hostname = request.host
-  # end	
+  def find_url
+  	@hostname = request.host
+  end	
 end
